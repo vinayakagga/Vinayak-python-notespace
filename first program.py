ab7a48263d1fef03d5b189d94224bb231f597088
vinayakagga <vinayakagga@gmail.com>
2026-01-02 12:10:39 +0530

    first

---
+++ first program.py
@@ -0,0 +1,2 @@
+print("we can print numbers yay : 3443" )
+print
---
+++ program 2.py
@@ -0,0 +1,4 @@
+a = int( input("give a number "))
+b = int( input ("give second number "))
+
+print (" okay here is you multiplication " , a*b)
